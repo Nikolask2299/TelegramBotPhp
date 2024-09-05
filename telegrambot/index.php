634c9abf91638d8a22b19497e831cd80346d4651
Nikolask2299 <nikolasff889@yandex.ru>
2024-09-05 23:02:38 +0400

    update

---
+++ telegrambot/index.php
@@ -0,0 +1,14 @@
+<?php
+   require_once "vendor/autoload.php";
+   use TelegramBot\Api\Client;
+
+   try {
+    $bot = new Client();
+
+
+   } catch (Exception $e) {
+
+   }
+
+
+    
---
+++ telegrambot/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3371583e354ab0e24a0ea0e82c0dd8c88ff8b65e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3371583e354ab0e24a0ea0e82c0dd8c88ff8b65e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'telegram-bot/api' => array(
+            'pretty_version' => 'v2.5.0',
+            'version' => '2.5.0.0',
+            'reference' => 'eaae3526223db49a1bad76a2dfa501dc287979cf',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../telegram-bot/api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
